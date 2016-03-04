81be81f56a7aa0631ae9c01c1af43932e716d8ec
jxhan <jxhan@Medivh.(none)>
2016-03-04 17:27:04 +0000

    version number update: bugfix for MPI message size limit

--- src/config_parser.h
+++ src/config_parser.h
@@ -10,7 +10,7 @@
 #include "datatypes.h"
 #include "mpi_wrapper.h"
 
-#define HBT_VERSION "1.6.3.MPI"
+#define HBT_VERSION "1.6.4.MPI"
 
 namespace PhysicalConst
 {//initialized after reading parameter file.
