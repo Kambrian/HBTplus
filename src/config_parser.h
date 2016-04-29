3422a43b38e8939620112dbf2ccab588e396a7d7
jxhan <jxhan@Medivh.(none)>
2016-04-29 14:30:34 +0100

    V1.7.0.MPI: batch binary search for particle query
    (renamed from V1.7.6.MPI)

--- src/config_parser.h
+++ src/config_parser.h
@@ -10,7 +10,7 @@
 #include "datatypes.h"
 #include "mpi_wrapper.h"
 
-#define HBT_VERSION "1.7.6.MPI"
+#define HBT_VERSION "1.7.0.MPI"
 
 namespace PhysicalConst
 {//initialized after reading parameter file.
