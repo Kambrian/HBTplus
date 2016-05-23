9850315c924813f7557eb0598632aecf6e275ede
jxhan <jxhan@Medivh.(none)>
2016-05-23 17:33:32 +0100

    bugfix in SpinBullock
    /sqrt(2GMR), not GmR

--- src/subhalo.cpp
+++ src/subhalo.cpp
@@ -214,7 +214,7 @@ void Subhalo_t::CalculateProfileProperties(const Snapshot_t &epoch)
   {
 	SpinPeebles[i]=SpecificAngularMomentum[i]*
 	  sqrt(fabs(SpecificSelfPotentialEnergy+SpecificSelfKineticEnergy))/PhysicalConst::G/Nbound/PartMass;
-	SpinBullock[i]=SpecificAngularMomentum[i]/sqrt(2.*PhysicalConst::G*PartMass*R2SigmaComoving);
+	SpinBullock[i]=SpecificAngularMomentum[i]/sqrt(2.*PhysicalConst::G*PartMass*Nbound*R2SigmaComoving);
   }
 #endif
 }
