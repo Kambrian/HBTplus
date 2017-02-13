4d2deed7ced2927cfa72202895ac45c1999fe7df
Jiaxin Han <hanjiaxin@gmail.com>
2017-02-13 14:54:07 +0900

    clear empty and duplicate snapshots

--- toolbox/CreateSnapshotList.py
+++ toolbox/CreateSnapshotList.py
@@ -3,19 +3,36 @@
 input snapshot path and the type to list: snap, snip or all (=snap+snip)
 '''
 import sys,os,glob
+import numpy as np
 
 rootdir=sys.argv[1]
 listtype=sys.argv[2] # snap, snip, all
 #rootdir='/cosma/home/jvbq85/data/HBT/data/apostle/S1_HR/'
 
-os.chdir(rootdir)
-snaplist=sorted(glob.glob('snapshot_*'))
-sniplist=sorted(glob.glob('snipshot_*'))
-
 def redshift(snapname):
   return float(snapname.split('_z')[1].replace('p','.'))
-  
-comlist=sorted(snaplist+sniplist, key=redshift, reverse=True)
+
+def clear_dup(snaps):
+  '''clear duplicate redshifts'''
+  return list(np.array(snaps)[np.unique([redshift(s) for s in snaps], return_index=True)[1]])
+
+def clear_empty(snaps):
+  '''clear empty dirs'''
+  return [x for x in snaps if os.listdir(x)!=[]]
+
+os.chdir(rootdir)
+snaplist=clear_empty(sorted(glob.glob('snapshot_*')))
+n0=len(snaplist)
+snaplist=clear_dup(snaplist)
+sniplist=clear_empty(sorted(glob.glob('snipshot_*')))
+n1=len(sniplist)
+sniplist=clear_dup(sniplist)
+
+comlist=sorted(list(snaplist)+list(sniplist), key=redshift, reverse=True)
+n2=len(comlist)
+comlist=clear_dup(comlist)
+sys.stderr.write("%d duplicates cleared\n"%(n0+n1+n2-len(snaplist)-len(sniplist)-len(comlist)))
+
 outlist={'snap':snaplist, 'snip':sniplist, 'all': comlist}
 
 for x in outlist[listtype]:
