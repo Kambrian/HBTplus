492843c02ab1c1a8b12817f3e07a08f578ef0cf8
Jiaxin Han <hanjiaxin@gmail.com>
2017-08-13 23:37:41 +0900

    more doc on the omp reduction test

--- src/test/test_ompreduction.cpp
+++ src/test/test_ompreduction.cpp
@@ -3,21 +3,21 @@
 #include <omp.h>
 using namespace std;
 
-#define N 100
+#define N 10
 int main()
 {
   int x[N];
   for(int i=0;i<N;i++)
     x[i]=i;
-  swap(x[0], x[30]);
+  swap(x[0], x[3]);
   int imin,imax;
   imin=imax=x[0];
-#pragma omp parallel for reduction(max: imax), reduction(min: imin)
+#pragma omp parallel for reduction(max: imax) reduction(min: imin)
   for(int i=1;i<N;i++)
   {
     if(x[i]>imax)
       imax=x[i];
-    if(x[i]<imin) //cannot use elseif here, because of the initialization of reduction vars
+    else if(x[i]<imin) //cannot use elseif here, because of the initialization of reduction vars. each thread will skip the first imin comparison.
       imin=x[i];
   }
   
