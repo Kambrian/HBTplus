3be7b6cb58099b2d859c9c044d7d7155bfeed104
Jiaxin Han <hanjiaxin@gmail.com>
2018-04-11 16:51:10 +0900

    v1.16.0.MPI: search API changed to use particle collector

--- src/config_parser.h
+++ src/config_parser.h
@@ -10,7 +10,7 @@
 #include "datatypes.h"
 #include "mpi_wrapper.h"
 
-#define HBT_VERSION "1.15.4.MPI"
+#define HBT_VERSION "1.16.0.MPI"
 
 namespace PhysicalConst
 {//initialized after reading parameter file.
