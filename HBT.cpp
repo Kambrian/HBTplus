ca39e9762e2283039ddc93ee39d631e0703aebe8
Jiaxin Han <jiaxin.han@sjtu.edu.cn>
2023-06-16 12:13:23 +0800

    compatibility improvement: replace deprecated omp_set_nested

--- HBT.cpp
+++ HBT.cpp
@@ -14,17 +14,18 @@ using namespace std;
 int main(int argc, char **argv)
 {
 #ifdef _OPENMP
- omp_set_nested(0);
+ //omp_set_nested(0);
+ omp_set_max_active_levels(1); //max_active_level 0: no para; 1: single layer;
 #endif
   int snapshot_start, snapshot_end;
   ParseHBTParams(argc, argv, HBTConfig, snapshot_start, snapshot_end);
   mkdir(HBTConfig.SubhaloPath.c_str(), 0755);
   HBTConfig.DumpParameters();
-	
+
   SubhaloSnapshot_t subsnap;
-  
+
   subsnap.Load(snapshot_start-1, SubReaderDepth_t::SrcParticles);
-  
+
   Timer_t timer;
   ofstream time_log(HBTConfig.SubhaloPath+"/timing.log", fstream::out|fstream::app);
   time_log<<fixed<<setprecision(1);//<<setw(8);
@@ -34,7 +35,7 @@ int main(int argc, char **argv)
 	ParticleSnapshot_t partsnap;
 	partsnap.Load(isnap);
 	subsnap.SetSnapshotIndex(isnap);
-	
+
 	HaloSnapshot_t halosnap;
 	halosnap.Load(isnap);
 	timer.Tick();
@@ -52,16 +53,16 @@ int main(int argc, char **argv)
 	timer.Tick();
 	subsnap.RefineParticles();
 	timer.Tick();
-	
+
 	subsnap.MergeSubhalos();
 	timer.Tick();
 
 	subsnap.UpdateTracks();
 // 	subsnap.ParticleIndexToId();//moved inside Save().
 	timer.Tick();
-	
+
 	subsnap.Save();
-	
+
 	timer.Tick();
 	time_log<<isnap;
 	for(int i=1;i<timer.Size();i++)
@@ -69,6 +70,6 @@ int main(int argc, char **argv)
 	time_log<<endl;
 	timer.Reset();
   }
-  
+
   return 0;
 }
