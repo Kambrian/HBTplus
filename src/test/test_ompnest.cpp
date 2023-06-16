372a6157f236387e0e2856017ee4e91acd69c228
Jiaxin Han <jiaxin.han@sjtu.edu.cn>
2023-06-16 12:11:16 +0800

    compatiblity improvement: replace deprecated omp_set_nested

--- src/test/test_ompnest.cpp
+++ src/test/test_ompnest.cpp
@@ -7,7 +7,8 @@ using namespace std;
 int main(int argc, char **argv)
 {
 int n=atoi(argv[1]);
-omp_set_nested(0);
+//omp_set_nested(0);
+omp_set_max_active_levels(1); //max_active_level 0: no para; 1: single layer;
 cout<<"InPara:"<<omp_in_parallel()<<", Level:"<<omp_get_level()<<", nThreads:"<<omp_get_num_threads()<<endl;
 cout<<"-----------------------------------\n";
 #pragma omp parallel num_threads(2) if(n)
