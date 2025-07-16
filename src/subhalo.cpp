2467b9a72b58f7721e053cf1e9a5a0bf9445b880
Jiaxin Han <jiaxin.han@sjtu.edu.cn>
2025-07-16 11:46:53 +0800

    trivial: fixed a typo in the comment of peebles spin

--- src/subhalo.cpp
+++ src/subhalo.cpp
@@ -74,16 +74,16 @@ inline bool CompProfVel(const RadMassVel_t &a, const RadMassVel_t &b)
 void Subhalo_t::CalculateProfileProperties(const ParticleSnapshot_t &part_snap)
 {
   /* to calculate the following density-profile related properties
-   *  currently only support const particle masses. 
-   * 
+   *  currently only support const particle masses.
+   *
   HBTReal RmaxComoving;
   HBTReal VmaxPhysical;
   HBTReal LastMaxVmax;
   HBTInt SnapshotIndexOfLastMaxVmax; //the snapshot when it has the maximum Vmax, only considering past snapshots.
-  
+
   HBTReal R2SigmaComoving;
   HBTReal RHalfComoving;
-  
+
   HBTReal R200CritComoving;
   HBTReal R200MeanComoving;
   HBTReal RVirComoving;
@@ -114,9 +114,9 @@ void Subhalo_t::CalculateProfileProperties(const ParticleSnapshot_t &part_snap)
   }
   auto &Cosmology=part_snap.Cosmology;
   HBTReal VelocityUnit=PhysicalConst::G/Cosmology.ScaleFactor;
-  
+
   const HBTxyz &cen=ComovingMostBoundPosition; //most-bound particle as center.
-  
+
   vector <RadMassVel_t> prof(Nbound);
   #pragma omp parallel if(Nbound>100)
   {
@@ -144,13 +144,13 @@ void Subhalo_t::CalculateProfileProperties(const ParticleSnapshot_t &part_snap)
   VmaxPhysical=sqrt(maxprof->v*VelocityUnit);
   RHalfComoving=prof[Nbound/2].r;
   R2SigmaComoving=prof[(HBTInt)(Nbound*0.955)].r;
-  
+
   HBTReal virialF_tophat, virialF_b200, virialF_c200;
   Cosmology.HaloVirialFactors(virialF_tophat, virialF_b200, virialF_c200);
 //   Cosmology.SphericalOverdensitySize(MVir, RVirComoving, virialF_tophat, prof);
   Cosmology.SphericalOverdensitySize(BoundM200Crit, BoundR200CritComoving, virialF_c200, prof);
 //   Cosmology.SphericalOverdensitySize(M200Mean, R200MeanComoving, virialF_b200, prof);
-  
+
   if(VmaxPhysical>=LastMaxVmaxPhysical)
   {
 	SnapshotIndexOfLastMaxVmax=part_snap.GetSnapshotIndex();
@@ -162,7 +162,7 @@ void Subhalo_t::CalculateProfileProperties(const ParticleSnapshot_t &part_snap)
   for(int i=0;i<3;i++)
   {
 	SpinPeebles[i]=SpecificAngularMomentum[i]*
-	  sqrt(fabs(SpecificSelfPotentialEnergy+0.5*SpecificSelfKineticEnergy))/PhysicalConst::G/Mbound;
+	  sqrt(fabs(0.5*SpecificSelfPotentialEnergy+SpecificSelfKineticEnergy))/PhysicalConst::G/Mbound;
 	SpinBullock[i]=SpecificAngularMomentum[i]/sqrt(2.*PhysicalConst::G*Mbound*R2SigmaComoving);
   }
   */
@@ -185,7 +185,7 @@ void Subhalo_t::CalculateShape(const ParticleSnapshot_t& part_snap)
 	return;
   }
   const HBTxyz &cen=ComovingMostBoundPosition; //most-bound particle as center.
-  
+
   double Ixx=0,Iyy=0, Izz=0, Ixy=0, Ixz=0, Iyz=0;
   double Ixxw=0,Iyyw=0, Izzw=0, Ixyw=0, Ixzw=0, Iyzw=0;
   #pragma omp parallel for reduction(+:Ixx,Iyy,Izz,Ixy,Ixz,Iyz,Ixxw,Iyyw,Izzw,Ixyw,Ixzw,Iyzw) if(Nbound>100)
@@ -211,7 +211,7 @@ void Subhalo_t::CalculateShape(const ParticleSnapshot_t& part_snap)
 	  Ixy+=dx*dy*m;
 	  Ixz+=dx*dz*m;
 	  Iyz+=dy*dz*m;
-	  
+
 	  HBTReal dr2=dx2+dy2+dz2;
 	  dr2/=m;//for mass weighting
 	  Ixxw+=dx2/dr2;
@@ -225,7 +225,7 @@ void Subhalo_t::CalculateShape(const ParticleSnapshot_t& part_snap)
   InertialTensorWeighted[0]=Ixxw; InertialTensorWeighted[1]=Ixyw; InertialTensorWeighted[2]=Ixzw; InertialTensorWeighted[3]=Iyyw; InertialTensorWeighted[4]=Iyzw; InertialTensorWeighted[5]=Izzw;
   for(auto && I: InertialTensor) I/=Mbound;
   for(auto && I: InertialTensorWeighted) I/=Mbound;
-#ifdef HAS_GSL  
+#ifdef HAS_GSL
   EigenAxis(Ixx, Ixy, Ixz, Iyy, Iyz, Izz, InertialEigenVector);
   EigenAxis(Ixxw, Ixyw, Ixzw, Iyyw, Iyzw, Izzw, InertialEigenVectorWeighted);
 #endif
@@ -246,7 +246,7 @@ struct CompareParticleType_t
 
 void Subhalo_t::CountParticleTypes(const ParticleSnapshot_t& part_snap)
 {
-#ifndef DM_ONLY  
+#ifndef DM_ONLY
 //   CompareParticleType_t comparator(part_snap);
 //   stable_sort(Particles.begin(), Particles.begin()+Nbound, comparator);//only sort the bound part
   for(int itype=0;itype<TypeMax;itype++)
@@ -287,7 +287,7 @@ void Subhalo_t::CountParticleTypes(const ParticleSnapshot_t& part_snap)
 	  MboundType[itype]+=part_snap.GetMass(p);
 	}
   }
-#endif  
+#endif
 }
 
 #ifdef DM_ONLY
@@ -323,7 +323,7 @@ HBTInt Subhalo_t::ParticleIdToIndex(const ParticleSnapshot_t& part_snap)
   Nbound=accumulate(begin(NboundType), end(NboundType), (HBTInt)0);
   Mbound=accumulate(begin(MboundType), end(MboundType), (HBTReal)0.);
   assert(Nbound==np_new);
-  
+
   it_end=Particles.end();
   for(;it!=it_end;++it)
   {
@@ -333,12 +333,12 @@ HBTInt Subhalo_t::ParticleIdToIndex(const ParticleSnapshot_t& part_snap)
   }
   HBTInt np_old=Particles.size();
   Particles.resize(np_new);
-  
+
   if(np_old>1&&np_new<=1)//consumed to death
 	SnapshotIndexOfDeath=part_snap.GetSnapshotIndex();
- 
+
   if(np_old!=np_new) cout<<np_old-np_new<<" outof "<<np_old<<" particles consumed for track "<<TrackId<<"\n";
-	
+
   return np_old-np_new;
 }
 #endif
